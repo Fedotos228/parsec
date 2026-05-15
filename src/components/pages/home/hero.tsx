78c14313f0b3d4c477f9ab3df0e5bf21b1d59097
Fedotos228 <tudor.onceanu@gmail.com>
2026-05-15 15:06:44 +0300

    feat: add header to footer component and restore Image component in hero section

--- src/components/pages/home/hero.tsx
+++ src/components/pages/home/hero.tsx
@@ -1,5 +1,6 @@
 import { HomeQuery } from '@/queries/home.queries'
 import { wpFetch } from '@/queries/wordpress'
+import Image from 'next/image'
 import { HeroItem } from './hero-item'
 
 export default async function Hero() {
@@ -8,14 +9,14 @@ export default async function Hero() {
 
   return (
     <div className='relative h-dvh w-full grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1'>
-      {/* <Image
+      <Image
         src='/Logo.svg'
         alt='SCP Parsec Logo'
         width={200}
         height={200}
         className='absolute top-1/2 left-1/2 -translate-1/2 z-30 h-20 w-20 md:w-52 md:h-52'
         priority
-      /> */}
+      />
       {/* <video autoPlay loop muted playsInline className='absolute inset-0 object-cover z-20 w-full h-full'>
         <source src="/video/hero.mp4" media="(min-width: 768px)" type="video/mp4" />
         <source src="/video/hero-mobile.mp4" media="(max-width: 767px)" type="video/mp4" />
